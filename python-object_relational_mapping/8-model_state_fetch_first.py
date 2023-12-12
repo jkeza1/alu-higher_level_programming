8b1fc3e70b028589a8126ec7bf09590a1a1be2af
keza <j.keza1@alustudent.com>
2023-12-12 09:42:57 -0800

    done

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""Prints the first state objects from the database hbtn_0e_6_usa."""
-
+""" prints the first State object from the
+database hbtn_0e_6_usa"""
 import sys
-from model_state import Base, State
-from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import Session
+from model_state import Base, State
 
 if __name__ == "__main__":
-    my_engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    Base.metadata.create_all(my_engine)
+    session = Session(engine)
 
-    my_session = Session(my_engine)
+    record = session.query(State).first()
 
-    state = my_session.query(State).first()
-
-    if state:
-        print("{}: {}".format(state.__dict__["id"], state.__dict__["name"]))
+    if record:
+        print("{}: {}".format(record.__dict__['id'], record.__dict__['name']))
     else:
         print("Nothing")
 
-    my_session.close()
+    session.close()
