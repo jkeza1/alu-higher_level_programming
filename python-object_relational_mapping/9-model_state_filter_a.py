3594bae2d02c006f74d16ceed642cfd1d3c50cee
keza <j.keza1@alustudent.com>
2023-12-12 09:45:08 -0800

    done

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script that lists all states with letter a"""
+"""A script that lists all records with letter a"""
 
 
 import sys
@@ -9,19 +9,19 @@ from sqlalchemy import create_engine
 
 
 if __name__ == "__main__":
-    my_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                         sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True
                     )
-    Session = sessionmaker(bind=my_engine)
+    Session = sessionmaker(bind=engine)
 
-    Base.metadata.create_all(my_engine)
+    Base.metadata.create_all(engine)
 
-    my_session = Session()
+    session = Session()
 
-    states = my_session.query(State).filter(State.name.contains('a')).all()
+    states = session.query(State).filter(State.name.contains('a')).all()
 
     for state in states:
         print(f'{state.id}: {state.name}')
 
-    my_session.close()
+    session.close()
