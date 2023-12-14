dcfaad7c6e59c73dbbfb62dca310f7ed0d54a84d
keza <j.keza1@alustudent.com>
2023-12-14 00:08:16 -0800

    done

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -10,10 +10,11 @@ from models.square import Square
 
 
 class TestBase(TestCase):
-    """The Test class for the Base class in models"""
+    """The Test class for the Base class in models."""
 
     def test_ba(self):
         """Test the starting point of creation of Base"""
+
         base = Base()
         base_1 = Base()
         base_89 = Base(89)
@@ -23,6 +24,7 @@ class TestBase(TestCase):
 
     def test_to_json_string(self):
         """Test the converting of lists to dicts"""
+
         self.assertEqual(Base.to_json_string(None), "[]")
         self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
         self.assertEqual(type(Base.to_json_string([{'id': 12}])), str)
@@ -30,43 +32,42 @@ class TestBase(TestCase):
 
     def test_save_to_file(self):
         """Test that the file saves list objects to  file"""
+
         Base._Base__nb_objects = 0
         Square.save_to_file(None)
 
         self.assertTrue(os.path.isfile("Square.json"))
-        with open("Square.json") as file:
-            self.assertEqual(file.read(), '[]')
+
+        with open("Square.json") as my_file:
+            self.assertEqual(my_file.read(), '[]')
 
         Square.save_to_file([])
-        with open("Square.json") as file:
-            self.assertEqual(file.read(), '[]')
-            self.assertEqual(type(file.read()), str)
 
-        Square.save_to_file([Square(1)])
-        with open("Square.json") as file:
-            self.assertEqual(file.read(),
-                             '[{"id": 1, "size": 1, "x": 0, "y": 0}]')
+        with open("Square.json") as my_file:
+            self.assertEqual(my_file.read(), '[]')
+            self.assertEqual(type(my_file.read()), str)
+
+        Square.save_to_file([Square(4)])
+        with open("Square.json") as my_file:
+            self.assertEqual(my_file.read(),
+                             '[{"id": 1, "size": 4, "x": 0, "y": 0}]')
         Base._Base__nb_objects = 0
 
         Rectangle.save_to_file(None)
         self.assertTrue(os.path.isfile("Rectangle.json"))
 
-        with open("Rectangle.json") as file:
-            self.assertEqual(file.read(), '[]')
+        with open("Rectangle.json") as my_file:
+            self.assertEqual(my_file.read(), '[]')
 
         Rectangle.save_to_file([])
-        with open("Rectangle.json") as file:
-            self.assertEqual(file.read(), '[]')
-            self.assertEqual(type(file.read()), str)
+        with open("Rectangle.json") as my_file:
+            self.assertEqual(my_file.read(), '[]')
+            self.assertEqual(type(my_file.read()), str)
 
-        Rectangle.save_to_file([Rectangle(2, 3)])
-        with open("Rectangle.json") as file:
-            self.assertEqual(file.read(),
-                             '[{"id": 1, "width": 2, '
-                             '"height": 3, "x": 0, "y": 0}]')
 
     def test_from_json_string(self):
-        """Test """
+        """Test the converting of a file from a JSON string representation."""
+
         self.assertEqual(Base.from_json_string(None), [])
         self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
         self.assertEqual(type(Base.from_json_string('[{"id": 89}]')), list)
